8de18c49ee769ad89c326c9283f7a9dcf14c2d8f
freoted <mrsrosiebell@gmail.com>
2015-10-03 20:43:20 +0800

    created new file to crawl oscar winners. Now prints on same line

--- imdb_wc_oscars.py
+++ imdb_wc_oscars.py
@@ -2,13 +2,13 @@
 # imdb web crawler
 
 #Future work:
-##Get rid of the 'x of y found this interesting'
+##Get rid of the 'x of y found this interesting' - DONE
 ##Change initial page to best picture winner from imdb instead of Top 250 movies.
-##Get name of the movie
+##Get name of the movie - DONE
 ##Use while loop to limit number of results (for testing) - DONE
 ##Get map format that calculates number of overlapping variables and displays that no
 ##on the map
-##Put each variable (movie name, link, location) in a new column, or seperated by comma
+##Put each variable (movie name, link, location) in a new column, or seperated by forward slash -DONE
 ##Export to excel?
 
 
@@ -25,7 +25,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #This page lists the current top 250 movies on imdb
-url = 'http://www.imdb.com/chart/top?ref_=nv_ch_250_4'
+url = 'http://www.imdb.com/search/title?count=100&groups=oscar_best_picture_winners&sort=year,desc&ref_=nv_ch_osc_3'
 
 #grabs all the text from this page, and makes it a BeautifulSoup object
 source_code = requests.get(url)
@@ -36,23 +36,16 @@ soup = BeautifulSoup(plain_text, "html.parser")
 link_count = 0
 
 #searches through BS4 object for all the title links
-for link in soup.find_all('td',{'class':'titleColumn'}):
+for link in soup.find_all('td',{'class':'title'}):
         #limits search results to first three links (testing purposes)
-        if link_count < 3:
+        if link_count < 5:
                 href = link.find('a').get('href')
                 #just print bit of link that you need
                 short_href = href[:17]
                 #http://www.imdb.com/title/tt0111161/locations?ref_=tt_dt_dt
                 #locationlink
                 location_link = 'http://www.imdb.com' + short_href + 'locations?ref_=tt_dt_dt'
-                #print (location_link)
-                #print movie title
-                print ("_________________________________________________________________")
                 title = link.find('a')
-                print (title.contents[0])
-                print (" ")
-                
-
                 location_source_code = requests.get(location_link)
                 location_plain_text = location_source_code.text
                 location_soup = BeautifulSoup(location_plain_text, "html.parser")
@@ -61,7 +54,7 @@ for link in soup.find_all('td',{'class':'titleColumn'}):
                 for loc_link in location_soup.find_all('div',{'id':'filming_locations_content'}):
                         for dt_links in loc_link.find_all('dt'):
                                 for loc_name in dt_links.find_all('a'):
-                                        print ((title.contents[0])+ " / " + (loc_name.contents[0]))
+                                        print ((title.contents[0])+ "/" + (loc_name.contents[0]))
                 link_count = link_count + 1
         else:
                 "program completed"
