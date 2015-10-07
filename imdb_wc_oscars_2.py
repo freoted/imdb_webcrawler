23d4c26b232332de537ba03b44aa90a826242299
freoted <mrsrosiebell@gmail.com>
2015-10-07 20:23:21 +0800

    updated with function

--- imdb_wc_oscars_2.py
+++ imdb_wc_oscars_2.py
@@ -17,15 +17,14 @@ def soupRecipe(url):
 def main():
 
         #sets a counter so only the first few links are printed (for testing purposes)
-        link_count = 0
         
-        #This page lists the current top 250 movies on imdb
-        url = 'http://www.imdb.com/search/title?count=100&groups=oscar_best_picture_winners&sort=year,desc&ref_=nv_ch_osc_3'
+        #This page lists the current best picture winning movies on imdb
+        best_pic_url = 'http://www.imdb.com/search/title?count=100&groups=oscar_best_picture_winners&sort=year,desc&ref_=nv_ch_osc_3'
 
-        soup = soupRecipe(url)
+        best_pic_soup = soupRecipe(best_pic_url)
                 
         #searches through BS4 object for all the title links
-        for link in soup.find_all('td',{'class':'title'}):
+        for link in best_pic_soup.find_all('td',{'class':'title'}):
                 href = link.find('a').get('href')
                 #just print bit of link that you need
                 short_href = href[:17]
